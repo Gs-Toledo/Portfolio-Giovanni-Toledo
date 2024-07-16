c966c700519f379aa8332f1f5195262b0e55cf0d
Giovanni Toledo <giovanni.toledo@injunior.com.br>
2024-07-16 12:17:32 -0300

    feat: parallax effect na home

--- portfolio/portfolio-gsToledo/src/App.vue
+++ portfolio/portfolio-gsToledo/src/App.vue
@@ -3,9 +3,18 @@
 
     <app-header />
 
-    <v-main>
-      Ola mundo
-    </v-main>
+    <v-parallax src="https://cdn.wallpapersafari.com/26/86/B0iRGn.jpg">
+      <div class="d-flex flex-column fill-height justify-center align-center text-white">
+        <h1 class="text-h2 font-weight-thin mb-4">
+          Bem vindo
+        </h1>
+        <h4 class="subheading">
+          Navegue pelo Portfolio, Sinta-se em casa!
+        </h4>
+      </div>
+    </v-parallax>
+
+    <v-main />
 
     <app-footer />
 
