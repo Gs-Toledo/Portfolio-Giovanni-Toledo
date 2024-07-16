596cc2ce1056e2b383152b9599776d545d637d12
Giovanni Toledo <giovanni.toledo@injunior.com.br>
2024-07-16 11:17:12 -0300

    first commit

---
+++ portfolio/portfolio-gsToledo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+
+    <app-header />
+
+    <v-main>
+      Ola mundo
+    </v-main>
+
+    <app-footer />
+
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
---
+++ portfolio/portfolio-gsToledo/src/components/AppHeader.vue
@@ -0,0 +1,56 @@
+<template>
+
+  <v-app-bar :elevation="2">
+
+    <template #prepend>
+      <v-app-bar-nav-icon />
+    </template>
+
+    <v-app-bar-title>
+
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          class="mx-2"
+          rounded="xl"
+          variant="text"
+        >
+          {{ link }}
+        </v-btn>
+      </v-row>
+
+    </v-app-bar-title>
+
+    <v-btn
+      class="mdi mdi-theme-light-dark"
+      density="compact"
+      rounded="xl"
+      size="small"
+      @click="toggleTheme"
+    />
+
+  </v-app-bar>
+
+</template>
+
+<script lang="ts" setup>
+
+  import { useTheme } from 'vuetify'
+
+  const theme = useTheme()
+
+  function toggleTheme () {
+    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  }
+
+  const links = [
+    'Home',
+    'Sobre',
+    'Projetos',
+    'Serviços',
+    'Blog',
+    'Fale Comigo',
+  ]
+
+</script>
